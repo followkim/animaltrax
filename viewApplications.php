777f0cf34d51afbb9770f57adade9e6572f9f40f
Kim <followkim@gmail.com>
2015-03-11 19:37:23 -0700

    Added search funtionality.

--- viewApplications.php
+++ viewApplications.php
@@ -23,7 +23,14 @@
 
 	$mysqli = DBConnect();
 
-// Clock if we need to open/close an application
+	// is this a POST if so, grab the POST varibales.
+	$isPost = ($_SERVER['REQUEST_METHOD'] == 'POST');
+	$name = $isPost?$_POST['name']:'';
+	$species = $isPost?$_POST['species']:'';
+	$breed = $isPost?$_POST['breed']:'';
+	$showClosed = (isset($_POST['showClosed'])?1:0);
+
+    // Check if we need to open/close an application (GET)
     if (isset($_GET['closed'])) {
         $shouldClose = $_GET['closed'];
         $applicationID = $_GET['applicationID'];
@@ -38,7 +45,30 @@
 
 ?>
 
-
+<form action="" method="POST">
+	<table id=criteria>
+		<?=trd_labelData("Name", $name, "name")?>
+		<tr>
+			<td style="text-align: right;">Species: </td>
+			<td style="text-align: left;">
+				 <select name=species>
+				  <option value=""></option>
+				  <option value="D" <?= ($species=='D'?"selected":"") ?>>Dog</option>
+				  <option value="C" <?= ($species=='C'?"selected":"") ?>>Cat</option>
+				  <option value="O" <?= ($species=='O'?"selected":"") ?>>Other</option>
+				</select> 		
+			</td>
+		</tr>
+		<?=trd_labelData("Breed", $breed, "breed")?>
+        <?=trd_labelChk("Show Closed?", "showClosed", $showClosed)?>
+		<tr>
+            <td align="right">
+				<input type="submit" value="Search"/>
+				<input type="submit" value="Clear" action="viewApplications.php" method="GET"/> <! TODO-- need to implement -->
+			</td>
+		</tr>
+	</table>
+</form>
 
 
 <!-- Show applications -->
@@ -55,7 +85,7 @@ Open Applications:
 	</tr>
 
 <?php
-		$sql = "select * from Application a inner join Person p on p.personID = a.personID where closed=0 order by applicationDate";
+		$sql = "CALL FindApplication('".lbt($name)."', '".lbt($breed)."', '".lbt($species)."', ".($showClosed).")";
 		$result = $mysqli->query($sql);
 		if (!$result) errorPage($mysqli->errno, $mysqli->error, $sql);
 
@@ -70,7 +100,7 @@ Open Applications:
 		<td><?= $row['breed'] ?>&nbsp;</td>
 		<td>
             <a href="addApplication.php?applicationID=<?=$row['applicationID']?>&personID=<?=$row['personID']?>">Edit</a>
-            <a href="viewApplications.php?applicationID=<?=$row['applicationID']?>&closed=1">Close</a>
+            <a href="viewApplications.php?applicationID=<?=$row['applicationID']?>&closed=<?=$row['closed']?0:1?>"><?=$row['closed']?"Open":"Close"?></a>
         </td>
 	</tr>
 <?php
@@ -79,48 +109,7 @@ Open Applications:
 ?>
 	<tr><td colspan=6>Found  <?=$result->num_rows?> <?=($result->num_rows==1?"application":"applications")?>.</td></tr>
 </table>
-<?php 	
-		$result->close();
-?>
 
-<!-- Show closed applications -->
-Closed Applications:
-<table  id="tabular" width="100%">
-	<tr>
-		<?=(isset($retPage)?"<th></th>":"")?>
-		<th>Date</th>
-		<th>Name</th>
-		<th>Species</th>
-        <th>Rank</th>
-        <th>Breed</th>
-		<th>&nbsp;</th>
-	</tr>
-
-<?php
-		$sql = "select * from Application a inner join Person p on p.personID = a.personID where closed=1 order by applicationDate";
-		$result = $mysqli->query($sql);
-		if (!$result) errorPage($mysqli->errno, $mysqli->error, $sql);
-
-		// Generate the table
-		while($row = $result->fetch_array()) {
-?>
-	<tr>
-		<td><?= MySql2Date($row['applicationDate']) ?>&nbsp;</td>
-		<td><a href=<?= "\"viewPerson.php?personID=".$row['personID']."\"" ?>><?= ($row['isOrg']?"":$row['firstName']." ") ?><?= $row['lastName'] ?></a>&nbsp;</td>
-		<td><?= ($row['species']=='D'?"Dog":"Cat")?>&nbsp;</td>
-		<td><?= $row['rank'] ?>&nbsp;</td>
-		<td><?= $row['breed'] ?>&nbsp;</td>
-		<td>
-            <a href="addApplication.php?applicationID=<?=$row['applicationID']?>&personID=<?=$row['personID']?>">Edit</a>
-            <a href="viewApplications.php?applicationID=<?=$row['applicationID']?>&closed=0">Open</a>
-        </td>
-	</tr>
-<?php
-	}
-	
-?>
-	<tr><td colspan=6>Found  <?=$result->num_rows?> <?=($result->num_rows==1?"application":"applications")?>.</td></tr>
-</table>
 <?php 	
 		$result->close();
 	pixie_footer(); 
