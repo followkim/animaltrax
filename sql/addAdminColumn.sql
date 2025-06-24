94c35730246261264821e8319acfe3ac64821c4b
Kimberley Gray <followkim@gmail.com>
2025-06-23 20:53:03 -0700

    Delete sql directory

--- sql/addAdminColumn.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE `pixie`.`Users` 
-ADD COLUMN `isAdmin` BIT NOT NULL AFTER `password`;
-
-update Users set isAdmin = 1 where userID = 1;
-update Users set isAdmin = 1 where userID = 2;
-
-
